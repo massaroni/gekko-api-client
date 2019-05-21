b6a6e78fee40c84dcfd60091ae55551b5e55d016
Kevin Massaroni <kevinmassaroni@gmail.com>
2019-05-20 23:04:32 -0400

    client pool should prioritize hosts from highest to lowest priority

--- test/gekko-client-pool.test.js
+++ test/gekko-client-pool.test.js
@@ -17,4 +17,21 @@ describe('Gekko Client Pool', function () {
     expect(gekkoClient.getTotalThreads()).to.equal(22);
   });
 
+  it('should prioritize hosts from highest to lowest and limit one request token per thread', async function () {
+    const GEKKO_HOSTS = [
+      { host: '192.168.1.10', threads: 2, strat: 'moon' },
+      { host: '192.168.1.11', threads: 1, strat: 'moon' }
+    ];
+    
+    const pool = new GekkoClientPool(GEKKO_HOSTS);
+    let token1 = await pool._getHostToken();
+    expect(token1.host.host).to.equal('192.168.1.10');
+
+    let token2 = await pool._getHostToken();
+    expect(token2.host.host).to.equal('192.168.1.10');
+
+    let token3 = await pool._getHostToken();
+    expect(token3.host.host).to.equal('192.168.1.11');
+  });
+
 });
